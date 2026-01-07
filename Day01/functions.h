83536bdcf8443c862c50ee64cd9cbfb2b7564545
mi <ogrinc.mirjam@gmail.com>
2026-01-07 11:21:00 +0100

    Delete Day01 directory

--- Day01/functions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-
-int rotacije(int trenutna, char smer, int sprememba);
-void parseRotation(const std::string& line, char& direction, int& value);
-int processInput(std::istream& in);
